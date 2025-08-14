441625a6d8ac3cddfb72e26c0b3427ca4ecfbc66
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-08-13 20:15:15 -0400

    suppress urrlib3 warnings

---
+++ src/datadec/__init__.py
@@ -0,0 +1,6 @@
+"""DataDecide library for downloading and processing ML experiment datasets."""
+
+import warnings
+
+# Suppress urllib3 LibreSSL warning on macOS
+warnings.filterwarnings("ignore", message="urllib3 v2 only supports OpenSSL")
